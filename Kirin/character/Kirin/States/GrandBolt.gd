be2dd4a7288bb42e616201d30fa58f4ebff39318
GardianOfHell <96070023+GardianOfHell@users.noreply.github.com>
2026-02-02 00:22:39 +0100

    Refactor GrandBolt and add DreadVestige projectile
    Replaced GrandBolt projectile assets and logic with new KirinWrym sprites and updated its state script. Added DreadVestige projectile scene and updated references in Kirin.tscn. Renamed ActionUI scene for DreadVestige and adjusted related resource paths.

--- Kirin/character/Kirin/States/GrandBolt.gd
+++ Kirin/character/Kirin/States/GrandBolt.gd
@@ -1,10 +1,3 @@
-extends SuperMove
+extends CharacterState
 
 
-
-const PROJECTILE_X = 20
-const PROJECTILE_Y = - -9
-
-func _frame_25():
-	var dir = xy_to_dir(data["x"], data["y"])
-	var obj = host.spawn_object(preload("res://Kirin/character/Kirin/Projectiles/GrandBolt.tscn"), PROJECTILE_X, PROJECTILE_Y, true, {"dir":dir})
