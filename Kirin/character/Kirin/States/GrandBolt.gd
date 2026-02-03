7f7c77871e48aef8ef558f37742bafa1022ce967
GardianOfHell <96070023+GardianOfHell@users.noreply.github.com>
2026-02-03 20:08:44 +0100

    Refactor GrandBolt projectile and state
    Replace SimpleProjectile with a generic ProjectileScene and update spawn parameters for GrandBolt. Changes include renaming and retyping exports (ProjectileScene, projPosX/projPosY, Spawn_Tick, Speed), moving spawn logic to use the new exports, and adjusting default directions/speed/lifetime. Kirin.tscn: set apply_fric, rename SimpleProjectileScene to ProjectileScene, add projPosX and static_x_dir. GrandBolt.tscn: enable apply_forces and disable active hit ticks (active_ticks = 0), remove legacy homing/bounce fields. GrandBoltproj.gd: rewritten from DirProjectileDefault to ObjectState with explicit movement, lifetime handling, ground collision/fizzle logic, hit handling and disabling. Overall this consolidates projectile handling and fixes spawn/motion behavior for the GrandBolt attack.

--- Kirin/character/Kirin/States/GrandBolt.gd
+++ Kirin/character/Kirin/States/GrandBolt.gd
@@ -1,21 +1,24 @@
 extends CharacterState
 
-
 export var _c_SimpleProjectile = 0
-export (PackedScene) var SimpleProjectileScene
-export var spawn_x_pos = 5
-export var spawn_y_pos = -10
-export var spawn_tick = 8
+export (PackedScene) var ProjectileScene
+export var projPosX = 32
+export var projPosY = -10
 export var static_x_dir = 1
 export var static_y_dir = 0
-export var speed = 5
-export var lifetime = 900
+export var Spawn_Tick = 7
+export var Speed = 30
+export var lifetime = 100
+
 
 func _tick():
-	if current_tick == spawn_tick:
+	if current_tick == Spawn_Tick:	
 		var dir_data = null
 		if data != null:
-			dir_data = {"dir":xy_to_dir(data.x, data.y),"speed":speed,"lifetime":lifetime}
+			dir_data = {"dir":xy_to_dir(data.x, data.y),"speed":Speed,"lifetime":lifetime}
 		else:
-			dir_data = {"dir":xy_to_dir(static_x_dir * host.get_facing_int(), static_y_dir),"speed":speed,"lifetime":lifetime}
-		host.spawn_object(SimpleProjectileScene, spawn_x_pos, spawn_y_pos, true, dir_data)
+			dir_data = {"dir":xy_to_dir(static_x_dir * host.get_facing_int(), static_y_dir),"speed":Speed,"lifetime":lifetime}	
+				
+		host.spawn_object(ProjectileScene, projPosX, projPosY, true, dir_data)
+	
+	
--- Kirin/character/Kirin/States/GrandBoltproj.gd
+++ Kirin/character/Kirin/States/GrandBoltproj.gd
@@ -1,1 +1,40 @@
-extends DirProjectileDefault
+extends ObjectState
+
+var hit_something = false
+var lifetime = 0 
+
+func _frame_0():
+	var dir = data["dir"]
+	lifetime = data["lifetime"]
+	host.set_grounded(false)
+	var move_vec = fixed.normalized_vec_times(str(dir.x), str(dir.y), str(data["speed"]))
+	host.apply_force(move_vec.x, move_vec.y)
+	host.sprite.rotation = float(fixed.vec_to_angle(data["dir"].x, data["dir"].y))
+	host.particles.rotation = float(fixed.vec_to_angle(data["dir"].x, data["dir"].y))
+
+
+
+func _tick():
+	var pos = host.get_pos()
+	host.update_grounded()
+	if current_tick > 1 and not hit_something and host.is_grounded() or pos.x < - host.stage_width or pos.x > host.stage_width:
+		fizzle()
+		pass
+	if current_tick > lifetime:
+		fizzle()
+		pass
+	elif not hit_something:
+		host.apply_forces()
+		host.update_data()
+
+func _got_parried():
+	fizzle()
+
+func fizzle():
+	hit_something = true
+	host.disable()
+
+func _on_hit_something(obj, hitbox):
+	._on_hit_something(obj, hitbox)
+	fizzle()
+
